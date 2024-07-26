5ca08c5e671d82c24f609b7efd217faa7cf2d3a5
asher7976 <97896895+asher7976@users.noreply.github.com>
2024-07-26 13:03:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ resource "aws_s3_bucket" "my_bucket7" {
 	Name          = "MyS3Buckets"
 	Environment    = "reProduction"
 }
+}
 
 resource "aws_dynamodb_table" "terraform_lock" {
   Name    = "terraform_lock"
